876281da349aefe7c0d9705c0a8db29bd688d73f
mohit00119 <122196525+mohit00119@users.noreply.github.com>
2024-04-28 16:38:39 +0530

    Voting System

---
+++ voting system.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     28/04/2024
+# Copyright:   (c) ASUS 2024
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#VOTING SYSTEM
+age=int(input("Enter your Age :"))
+if age >= 18:
+    print("You are eligible to Vote")
+else:
+    print("You are not eligible to vote")
